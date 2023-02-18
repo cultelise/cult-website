3cd1afb5f7ba2dac7ee771e12cd945cd29fd6940
elise <115508722+cultelise@users.noreply.github.com>
2023-02-18 16:57:23 -0600

    edit styling title/button

--- static/index.js
+++ static/index.js
@@ -1,17 +1,30 @@
-var button1 = document.getElementById('post-button-1');
-button1.addEventListener('click', function (e) {
-    var button = e.target;
+var postSymbol1 = document.getElementById('post-button-1');
+var postTitle1 = document.getElementById('post-title-1');
+var buttonHandler = function (e) {
+    var target = e.target;
+    var button;
+    if (target.id == 'post-button-1') {
+        button = e.target;
+    }
+    else {
+        button = postSymbol1;
+    }
     button.style.opacity = '0';
+    button.style.fontSize = '30px';
     setTimeout(function () {
         {
             if (button.textContent == '☽') {
                 button.textContent = '☼';
+                button.style.fontSize = '40px';
                 button.style.opacity = '100';
             }
             else {
                 button.textContent = '☽';
+                button.style.fontSize = '40px';
                 button.style.opacity = '100';
             }
         }
     }, 200);
-});
+};
+postSymbol1.addEventListener('click', buttonHandler);
+postTitle1.addEventListener('click', buttonHandler);
--- static/index.ts
+++ static/index.ts
@@ -1,15 +1,28 @@
-const button1 = document.getElementById('post-button-1') as HTMLButtonElement;
+const postSymbol1 = document.getElementById('post-button-1') as HTMLButtonElement;
+const postTitle1 = document.getElementById('post-title-1') as HTMLButtonElement
 
-
-button1.addEventListener('click', (e: Event) => {
-  const button = e.target as HTMLButtonElement
+const buttonHandler = (e: Event) => {
+  const target = e.target as HTMLButtonElement
+  let button: HTMLButtonElement
+  if (target.id == 'post-button-1') {
+    button = e.target as HTMLButtonElement
+  } else {
+    button = postSymbol1
+  }
   button.style.opacity = '0'
+  button.style.fontSize = '30px';
   setTimeout(() => {
     {if (button.textContent == '☽') {
     button.textContent = '☼'
+    button.style.fontSize = '40px';
     button.style.opacity = '100'
   } else {
     button.textContent = '☽'
+    button.style.fontSize = '40px';
     button.style.opacity = '100'
   }}}, 200)
-});
+}
+
+postSymbol1.addEventListener('click', buttonHandler);
+postTitle1.addEventListener('click', buttonHandler)
+
