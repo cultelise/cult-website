92e76884def1d471b1114349d36bb8aeb6fefa8f
elise <115508722+cultelise@users.noreply.github.com>
2023-02-18 18:24:25 -0600

    add post shadow

--- static/index.js
+++ static/index.js
@@ -1,5 +1,6 @@
 var postSymbol1 = document.getElementById('post-button-1');
 var postTitle1 = document.getElementById('post-title-1');
+var post1 = document.getElementById('post-1');
 var buttonHandler = function (e) {
     var target = e.target;
     var button;
@@ -11,18 +12,30 @@ var buttonHandler = function (e) {
     }
     button.style.opacity = '0';
     button.style.fontSize = '30px';
+    if (button.textContent == '☽') {
+        post1.style.borderTop = '2px solid whitesmoke';
+    }
+    else {
+        post1.style.borderBottom = '2px solid whitesmoke';
+    }
     setTimeout(function () {
-        {
-            if (button.textContent == '☽') {
-                button.textContent = '☼';
-                button.style.fontSize = '40px';
-                button.style.opacity = '100';
-            }
-            else {
-                button.textContent = '☽';
-                button.style.fontSize = '40px';
-                button.style.opacity = '100';
-            }
+        if (button.textContent == '☽') {
+            button.textContent = '☼';
+            button.style.fontSize = '40px';
+            button.style.opacity = '100';
+            setTimeout(function () {
+                post1.style.borderBottom = '2px solid black';
+                post1.style.borderTop = '2px solid whitesmoke';
+            }, 100);
+        }
+        else {
+            setTimeout(function () {
+                post1.style.borderBottom = '2px dashed whitesmoke';
+                post1.style.borderTop = '2px solid black';
+            }, 100);
+            button.textContent = '☽';
+            button.style.fontSize = '40px';
+            button.style.opacity = '100';
         }
     }, 200);
 };
--- static/index.ts
+++ static/index.ts
@@ -1,28 +1,44 @@
-const postSymbol1 = document.getElementById('post-button-1') as HTMLButtonElement;
-const postTitle1 = document.getElementById('post-title-1') as HTMLButtonElement
+const postSymbol1 = document.getElementById(
+  'post-button-1'
+) as HTMLButtonElement;
+const postTitle1 = document.getElementById('post-title-1') as HTMLButtonElement;
+const post1 = document.getElementById('post-1') as HTMLButtonElement;
 
 const buttonHandler = (e: Event) => {
-  const target = e.target as HTMLButtonElement
-  let button: HTMLButtonElement
+  const target = e.target as HTMLButtonElement;
+  let button: HTMLButtonElement;
   if (target.id == 'post-button-1') {
-    button = e.target as HTMLButtonElement
+    button = e.target as HTMLButtonElement;
   } else {
-    button = postSymbol1
+    button = postSymbol1;
   }
-  button.style.opacity = '0'
+  button.style.opacity = '0';
   button.style.fontSize = '30px';
-  setTimeout(() => {
-    {if (button.textContent == '☽') {
-    button.textContent = '☼'
-    button.style.fontSize = '40px';
-    button.style.opacity = '100'
+  if (button.textContent == '☽') {
+    post1.style.borderTop = '2px solid whitesmoke';
   } else {
-    button.textContent = '☽'
-    button.style.fontSize = '40px';
-    button.style.opacity = '100'
-  }}}, 200)
-}
+    post1.style.borderBottom = '2px solid whitesmoke';
+  }
+  setTimeout(() => {
+    if (button.textContent == '☽') {
+      button.textContent = '☼';
+      button.style.fontSize = '40px';
+      button.style.opacity = '100';
+      setTimeout(() => {
+        post1.style.borderBottom = '2px solid black';
+        post1.style.borderTop = '2px solid whitesmoke';
+      }, 100);
+    } else {
+      setTimeout(() => {
+        post1.style.borderBottom = '2px dashed whitesmoke';
+        post1.style.borderTop = '2px solid black';
+      }, 100);
+      button.textContent = '☽';
+      button.style.fontSize = '40px';
+      button.style.opacity = '100';
+    }
+  }, 200);
+};
 
 postSymbol1.addEventListener('click', buttonHandler);
-postTitle1.addEventListener('click', buttonHandler)
-
+postTitle1.addEventListener('click', buttonHandler);
