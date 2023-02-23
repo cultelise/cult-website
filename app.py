f4eb36c4f42e8c8cf550990ae3d0496193edc5e7
elise <115508722+cultelise@users.noreply.github.com>
2023-02-23 05:03:10 -0600

    add variable to index.html

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 title = "Blog Post Title"
+preview = "Preview Text"
 
 
 @app.route("/")
 def hello_elise():
-    return render_template("index.html", title=title)
+    return render_template("index.html", title=title, preview=preview)
 
 
 if __name__ == "__main__":
